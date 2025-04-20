1a45bcb5d11fed0e986ce74fab6f6cc1cfd51497
Ahmad Fauzi <fauzitkj3@gmail.com>
2025-04-20 21:56:49 +0700

    feat(server): get stock managements

--- server/controllers/stock.controller.js
+++ server/controllers/stock.controller.js
@@ -1,8 +1,79 @@
-const { Stock, Variant, Sequelize } = require("../models");
-const fs = require("fs");
-const { Op } = Sequelize;
+const { Stock, Variant, Category, Product, Sequelize } = require("../models");
+const { Op, literal } = Sequelize;
 
 let self = {};
+
+self.getManagements = async (req, res, next) => {
+  try {
+    const { name, categoryId, status = "all" } = req.query;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 15;
+    const offset = (page - 1) * limit;
+
+    const whereProduct = {};
+    const whereCondition = { track_stock: 1 };
+
+    if (name) {
+      whereProduct.name = { [Op.like]: `%${name}%` };
+    }
+
+    if (categoryId) {
+      whereProduct.categoryId = categoryId;
+    }
+
+    if (status === "low") {
+      whereCondition.stock = {
+        [Op.gt]: 0,
+        [Op.lte]: literal("minimum_stock"),
+      };
+    } else if (status === "out") {
+      whereCondition.stock = { [Op.lte]: 0 };
+    } else if (status === "all") {
+      whereCondition[Op.or] = [
+        {
+          stock: {
+            [Op.gt]: 0,
+            [Op.lte]: literal("minimum_stock"),
+          },
+        },
+        { stock: { [Op.lte]: 0 } },
+      ];
+    }
+
+    const { count, rows } = await Variant.findAndCountAll({
+      where: whereCondition,
+      include: [
+        {
+          model: Product,
+          as: "product",
+          attributes: ["id", "name"],
+          where: whereProduct,
+          include: [
+            {
+              model: Category,
+              as: "categories",
+              attributes: ["name"],
+            },
+          ],
+        },
+      ],
+      limit,
+      offset,
+      order: [["stock", "ASC"]],
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Data found",
+      data: rows,
+      totalData: count,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 self.getAdjustments = async (req, res, next) => {
   try {
--- server/routes/stock.routes.js
+++ server/routes/stock.routes.js
@@ -5,5 +5,6 @@ router
   .route("/adjustments")
   .get(stock.getAdjustments)
   .post(stock.createAdjustment);
+router.route("/managements").get(stock.getManagements);
 
 module.exports = router;
